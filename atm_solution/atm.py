38bfc6e2947fd64f4e2bc0969f25c73362a626e4
Moh'd Salem <m_saady_salem@msn.com>
2018-02-24 16:12:13 +0000

    Modify: Adjust args in doc_string

--- atm_solution/atm.py
+++ atm_solution/atm.py
@@ -7,6 +7,7 @@ def withdraw(limit, request):
 
     Arg:
        limit: Money limit within ATM machine during client withdraw operation.
+       request: The request a mount by the client.
 
     Returns:
        limit: The updated money limit after client withdraw operations.
