67ff18027fea5e086b6e11878a289bd6375fef12
Moh'd Salem <m_saady_salem@msn.com>
2018-02-24 06:39:54 +0000

    Modify: Remove extra "return", Add -ve case check

--- atm_solution/atm.py
+++ atm_solution/atm.py
@@ -10,41 +10,42 @@ def withdraw(limit, request):
        withdraw_req: The request a mount by the client.
 
     Returns:
-       new_limit: The updated money limit after client withdraw operations.
+       limit: The updated money limit after client withdraw operations.
     """
-
-    new_limit = limit
-    if request > limit:
-        print "The requested money is not available at the moment."
-        return new_limit
 
     money_unit = 100
     while request > 0:
-        if request - money_unit > 0:
+        if request > limit:
+            print "The requested money is not available at the moment."
+            break
+        if request - money_unit >= 0:
             request -= money_unit
-            new_limit -= money_unit
+            limit -= money_unit
             print "give %s" % money_unit
         elif money_unit == 100:
             money_unit = 50
-            continue
         elif money_unit == 50:
             money_unit = 10
-            continue
         elif money_unit == 10:
             money_unit = 5
-            continue
         else:
             print "give %s" % request
-            new_limit -= request
+            limit -= request
             request = 0
-    return new_limit
+
+    if request < 0:
+        print "The requested money is invalid amount."
+
+    return limit
 
 
 # Test cases
 # new_limit = withdraw(500, 277)
 # new_limit = withdraw(500, 600)
 # new_limit = withdraw(300, 273)
-new_limit = withdraw(500, 2)
+# new_limit = withdraw(500, 500)
+# new_limit = withdraw(500, 2)
 # new_limit = withdraw(0, 277)
-# new_limit = withdraw(500, -20)
+new_limit = withdraw(500, -20)
+
 print "The current balance: %d$ " % new_limit
