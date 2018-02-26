40c884fff6f2c1c17058c591c7ea0f76edf0d492
Moh'd Salem <m_saady_salem@msn.com>
2018-02-26 00:12:52 +0000

    Refactor: encapsulation into class

--- atm_solution/atm_refactored.py
+++ atm_solution/atm_refactored.py
@@ -1,63 +1,84 @@
-#!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 
 
-def validate_withdraw(balance, request):
-    """Checking if the requested amount is valid within the range of the current balance.
+class ATM():
 
-    Args:
-        balance: Current balance.
-        request: The request a mount by the client.
+    def __init__(self, balance, bank_name):
+        """Construct an ATM machine object.
 
-    Returns:
-        valid: Holds the validity
-    """
+        Args:
+            balance (int): Current available balance  befor withdraw operation
+            bank_name (str): The bank name which ATM machine belongs.
+        """
 
-    valid = True
-    if request <= 0:
-        print "The requested money is invalid amount."
-        valid = False
-    elif request > balance:
-        print "The requested money is above your current blance."
-        valid = False
-    return valid
+        self.balance = balance
+        self.bank_name = bank_name
 
+    def validate_withdraw(self, request):
+        """Checking if the requested amount is valid within the range of the current balance.
 
-def withdraw(balance, request, money_units=[100, 50, 10, 5, 4, 3, 2, 1]):
-    """Calculating how many currency papers the client required within the ATM machine according to current acount balance.
+        Args:
+            request (int): The request a mount by the client.
 
-    Arg:
-       balance: Current balance befor client withdraw operation.
-       request: The request a mount by the client.
-       money_units: A list of available currency papers.
+        Returns:
+            valid (bool): Holds the validity
+        """
 
-    Returns:
-       balance: The updated account balance after client withdraw operations.
-    """
+        valid = True
+        if request <= 0:
+            print "The requested amount is invalid.\n"
+            valid = False
+        elif request > self.balance:
+            print "The requested amount is above current balance.\n"
+            valid = False
+        return valid
 
-    print "Your current Balance: %d$ \n" % balance
-    i = 0
-    while request > 0:
-        count = request // money_units[i]
-        for j in range(count):
-            print "give %s" % money_units[i]
-        request -= count * money_units[i]
-        balance -= count * money_units[i]
-        i += 1
-    print "\nThe current balance after operations: %d$ " % balance
-    return balance
+    def withdraw(self, request, money_units=[100, 50, 10, 5, 4, 3, 2, 1]):
+        """Calculating how many currency papers the client required within the current balance in ATM machine.
+
+        Arg:
+           request (int): The request a mount by the client.
+           money_units (list): A list of available currency papers.
+
+        Returns:
+           balance (int): The updated account balance after client withdraw operations.
+        """
+
+        print "\n" + "=" * 25 + "\nWelcome to %s\nCurrent balance = %d$\n" % (
+            self.bank_name, self.balance) + "=" * 25
+        valid = self.validate_withdraw(request)
+        i = 0
+        while request > 0 and valid:
+            count = request // money_units[i]
+            for j in range(count):
+                print "give %s" % money_units[i]
+            request -= count * money_units[i]
+            self.balance -= count * money_units[i]
+            i += 1
+        return self.balance
 
 
 # Test cases
-# balance, request = 500, 277
-# balance, request = 500, 600
-# balance, request = 300, 273
-# balance, request = 500, 500
-# balance, request = 500, 2
-# balance, request = 0, 277
-# balance, request = 500, -20
-# balance, request = -2, 0
-balance, request = -2, -3
+balance1, balance2 = 500, 1000
+request1, request2 = 277, 1200
 
-if validate_withdraw(balance, request):
-    withdraw(balance, request)
+# balance1, balance2 = 500, 500
+# request1, request2 = 273, 500
+
+# balance1, balance2 = 500, 500
+# request1, request2 = 273, 500
+
+# balance1, balance2 = 500, 0
+# request1, request2 = 2, 277
+
+# balance1, balance2 = 500, -2
+# request1, request2 = -20, 0
+
+# balance1, balance2 = -2, 0
+# request1, request2 = -3, 0
+
+atm1 = ATM(balance1, "Smart Bank")
+atm2 = ATM(balance2, "Baraka Bank")
+
+atm1.withdraw(request1)
+atm2.withdraw(request2)
