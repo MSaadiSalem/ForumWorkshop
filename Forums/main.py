b8021aad3bed12bca581f72039c02acfe4fb8bd4
Moh'd Salem <m_saady_salem@msn.com>
2018-03-01 10:02:24 +0000

    Chore: update main module

--- Forums/main.py
+++ Forums/main.py
@@ -2,9 +2,16 @@
 # -*- coding: utf-8 -*-
 
 import models
+import stores
 
 member1 = models.Member("Member 01", 25)
 member2 = models.Member("Member 02", 35)
+
+member_store = stores.MemberStore()
+
+member_store.add(member1)
+member_store.add(member2)
+print(member_store.get_all())
 
 post1 = models.Post("Title 01", "Body contents", member1.member_id)
 post2 = models.Post("Title 02", "Body contents", member2.member_id)
