194bde146629a80c03034ad359c1ef934849fb12
Moh'd Salem <m_saady_salem@msn.com>
2018-03-01 12:04:17 +0000

    Chore: update stores module
    Add more instance method - workshop 18

--- Forums/stores.py
+++ Forums/stores.py
@@ -17,6 +17,25 @@ class MemberStore(object):
 
     def add(self, member):
         MemberStore.members += [member]
+        member.member_id = MemberStore.last_id
+        MemberStore.last_id += 1
+
+    def entity_exists(self, member):
+        exist = True
+        if self.get_by_id(member.member_id) == None:
+            exist = False
+        return exist
+
+    def get_by_id(self, id):
+        for member in MemberStore.members:
+            if member.member_id == id:
+                return member
+        print "Invalid Member ID."
+
+    def delete(self, id):
+        exist = self.get_by_id(id)
+        if exist != None:
+            MemberStore.members.remove(exist)
 
 
 class PostStore(object):
@@ -33,3 +52,14 @@ class PostStore(object):
 
     def add(self, post):
         PostStore.posts += [post]
+
+    def get_by_id(self, id):
+        for post in PostStore.posts:
+            if post.post_id == id:
+                return post
+        print "Invalid Post ID."
+
+    def delete(self, id):
+        exist = self.get_by_id(id)
+        if exist != None:
+            PostStore.posts.remove(exist)
